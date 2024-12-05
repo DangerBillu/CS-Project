74d1186f4e53d7e78bd5429a2e7d2e4143916a2f
DangerBillu <arnav.goyal@ais.amity.edu>
2024-12-05 18:25:44 +0530

    added file work

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': '', #your password
+    'password': '', #my password
     'database': '' #databse name
 }
 
